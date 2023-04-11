beace8517147cea5222cbb413536fb7a7a742f54
thapakorn odonrak <boatzarock01@gmail.com>
2023-04-11 10:18:49 +0700

    edit shop

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -9,7 +9,7 @@ export default function Shop() {
       <Appbar />
       <div>
               <h1 className="text-3xl ml-[3rem] mt-[1rem]">Shop</h1>
-        <div className="flex justify-center items-center flex-row flex-warp mt-[1rem]">
+        <div className="flex justify-center items-center  flex-warp mt-[1rem]" id ={styles.ShopBuy}>
     
         <div className="  w-[17rem] h-[22rem] bg-lime-300">
         <div className="flex justify-center items-center mt-[0.5rem]">
@@ -29,7 +29,7 @@ export default function Shop() {
    
 
  
-           <div className="  w-[17rem] h-[22rem] bg-lime-300 ml-[1rem]">
+           <div className="  w-[17rem] h-[22rem] bg-lime-300 ml-[1rem] "id ={styles.ShopBuy}>
            <div className="flex justify-center items-center mt-[0.5rem]">
         <button className=" w-[6rem] h-[3rem] text-white bg-lime-300 rounded-lg border border-white hover:bg-slate-200">
                   Shop Now{" "}
@@ -43,7 +43,7 @@ export default function Shop() {
               />
 
            </div >
-           <div className="  w-[17rem] h-[22rem] bg-lime-300 ml-[1rem]">
+           <div className="  w-[17rem] h-[22rem] bg-lime-300 ml-[1rem]"id ={styles.ShopBuy}>
            <div className="flex justify-center items-center mt-[0.5rem]">
         <button className=" w-[6rem] h-[3rem] text-white bg-lime-300 rounded-lg border border-white hover:bg-slate-200">
                   Shop Now{" "}
@@ -57,7 +57,7 @@ export default function Shop() {
               />
 
            </div >
-           <div className="  w-[17rem] h-[22rem] bg-lime-300 ml-[1rem]">
+           <div className="  w-[17rem] h-[22rem] bg-lime-300 ml-[1rem]"id ={styles.ShopBuy}>
            <div className="flex justify-center items-center mt-[0.5rem]">
         <button className=" w-[6rem] h-[3rem] text-white bg-lime-300 rounded-lg border border-white hover:bg-slate-200">
                   Shop Now{" "}
@@ -71,7 +71,7 @@ export default function Shop() {
               />
 
            </div >
-           <div className="  w-[17rem] h-[22rem] bg-lime-300 ml-[1rem]">
+           <div className="  w-[17rem] h-[22rem] bg-lime-300 ml-[1rem]"id ={styles.ShopBuy}>
            <div className="flex justify-center items-center mt-[0.5rem]">
         <button className=" w-[6rem] h-[3rem] text-white bg-lime-300 rounded-lg border border-white hover:bg-slate-200">
                   Shop Now{" "}
@@ -85,115 +85,113 @@ export default function Shop() {
               />
 
            </div >
-           <div className="  w-[17rem] h-[22rem] bg-lime-300 ml-[1rem]">
+           <div className="  w-[17rem] h-[22rem] bg-lime-300 ml-[1rem]"id ={styles.ShopBuy}>
            <div className="flex justify-center items-center mt-[0.5rem]">
         <button className=" w-[6rem] h-[3rem] text-white bg-lime-300 rounded-lg border border-white hover:bg-slate-200">
                   Shop Now{" "}
                 </button>
                 </div>
         <Image
-                src="/image 16.png"
+                src="/image 1.png"
                 alt=""
                 width={300}
                 height={300}
               />
 
            </div >
-
 
 
         </div>
-        <div className="flex justify-center items-center flex-row flex-warp mt-[1rem]">
+        <div className="flex justify-center items-center  flex-warp mt-[1rem]" id ={styles.ShopBuy}>
     
-        <div className="  w-[17rem] h-[22rem] bg-lime-300">
-        <div className="flex justify-center items-center mt-[0.5rem]">
-        <button className=" w-[6rem] h-[3rem] text-white bg-lime-300 rounded-lg border border-white hover:bg-slate-200">
-                  Shop Now{" "}
-                </button>
-                </div>
-        <Image
-                src="/t.png"
-                alt=""
-                width={300}
-                height={300}
-              />
+    <div className="  w-[17rem] h-[22rem] bg-lime-300">
+    <div className="flex justify-center items-center mt-[0.5rem]">
+    <button className=" w-[6rem] h-[3rem] text-white bg-lime-300 rounded-lg border border-white hover:bg-slate-200">
+              Shop Now{" "}
+            </button>
+            </div>
+    <Image
+            src="/image 16.png"
+            alt=""
+            width={300}
+            height={300}
+          />
 
-           </div >
-         
-   
+       </div >
+     
 
- 
-           <div className="  w-[17rem] h-[22rem] bg-lime-300 ml-[1rem]">
-           <div className="flex justify-center items-center mt-[0.5rem]">
-        <button className=" w-[6rem] h-[3rem] text-white bg-lime-300 rounded-lg border border-white hover:bg-slate-200">
-                  Shop Now{" "}
-                </button>
-                </div>
-        <Image
-                src="/image 1.png"
-                alt=""
-                width={300}
-                height={300}
-              />
 
-           </div >
-           <div className="  w-[17rem] h-[22rem] bg-lime-300 ml-[1rem]">
-           <div className="flex justify-center items-center mt-[0.5rem]">
-        <button className=" w-[6rem] h-[3rem] text-white bg-lime-300 rounded-lg border border-white hover:bg-slate-200">
-                  Shop Now{" "}
-                </button>
-                </div>
-        <Image
-                src="/image 4.png"
-                alt=""
-                width={300}
-                height={300}
-              />
 
-           </div >
-           <div className="  w-[17rem] h-[22rem] bg-lime-300 ml-[1rem]">
-           <div className="flex justify-center items-center mt-[0.5rem]">
-        <button className=" w-[6rem] h-[3rem] text-white bg-lime-300 rounded-lg border border-white hover:bg-slate-200">
-                  Shop Now{" "}
-                </button>
-                </div>
-        <Image
-                src="/t.png"
-                alt=""
-                width={300}
-                height={300}
-              />
+       <div className="  w-[17rem] h-[22rem] bg-lime-300 ml-[1rem] "id ={styles.ShopBuy}>
+       <div className="flex justify-center items-center mt-[0.5rem]">
+    <button className=" w-[6rem] h-[3rem] text-white bg-lime-300 rounded-lg border border-white hover:bg-slate-200">
+              Shop Now{" "}
+            </button>
+            </div>
+    <Image
+            src="/image 1.png"
+            alt=""
+            width={300}
+            height={300}
+          />
 
-           </div >
-           <div className="  w-[17rem] h-[22rem] bg-lime-300 ml-[1rem]">
-           <div className="flex justify-center items-center mt-[0.5rem]">
-        <button className=" w-[6rem] h-[3rem] text-white bg-lime-300 rounded-lg border border-white hover:bg-slate-200">
-                  Shop Now{" "}
-                </button>
-                </div>
-        <Image
-                src="/image 1.png"
-                alt=""
-                width={300}
-                height={300}
-              />
+       </div >
+       <div className="  w-[17rem] h-[22rem] bg-lime-300 ml-[1rem]"id ={styles.ShopBuy}>
+       <div className="flex justify-center items-center mt-[0.5rem]">
+    <button className=" w-[6rem] h-[3rem] text-white bg-lime-300 rounded-lg border border-white hover:bg-slate-200">
+              Shop Now{" "}
+            </button>
+            </div>
+    <Image
+            src="/t.png"
+            alt=""
+            width={300}
+            height={300}
+          />
 
-           </div >
-           <div className="  w-[17rem] h-[22rem] bg-lime-300 ml-[1rem]">
-           <div className="flex justify-center items-center mt-[0.5rem]">
-        <button className=" w-[6rem] h-[3rem] text-white bg-lime-300 rounded-lg border border-white hover:bg-slate-200">
-                  Shop Now{" "}
-                </button>
-                </div>
-        <Image
-                src="/image 4.png"
-                alt=""
-                width={300}
-                height={300}
-              />
+       </div >
+       <div className="  w-[17rem] h-[22rem] bg-lime-300 ml-[1rem]"id ={styles.ShopBuy}>
+       <div className="flex justify-center items-center mt-[0.5rem]">
+    <button className=" w-[6rem] h-[3rem] text-white bg-lime-300 rounded-lg border border-white hover:bg-slate-200">
+              Shop Now{" "}
+            </button>
+            </div>
+    <Image
+            src="/image 1.png"
+            alt=""
+            width={300}
+            height={300}
+          />
 
-           </div >
-       
+       </div >
+       <div className="  w-[17rem] h-[22rem] bg-lime-300 ml-[1rem]"id ={styles.ShopBuy}>
+       <div className="flex justify-center items-center mt-[0.5rem]">
+    <button className=" w-[6rem] h-[3rem] text-white bg-lime-300 rounded-lg border border-white hover:bg-slate-200">
+              Shop Now{" "}
+            </button>
+            </div>
+    <Image
+            src="/image 4.png"
+            alt=""
+            width={300}
+            height={300}
+          />
+
+       </div >
+       <div className="  w-[17rem] h-[22rem] bg-lime-300 ml-[1rem]"id ={styles.ShopBuy}>
+       <div className="flex justify-center items-center mt-[0.5rem]">
+    <button className=" w-[6rem] h-[3rem] text-white bg-lime-300 rounded-lg border border-white hover:bg-slate-200">
+              Shop Now{" "}
+            </button>
+            </div>
+    <Image
+            src="/image 1.png"
+            alt=""
+            width={300}
+            height={300}
+          />
+
+       </div >
 
 
     </div>
